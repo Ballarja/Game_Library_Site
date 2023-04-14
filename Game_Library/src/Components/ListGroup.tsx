67ca4d6e21fda6494ed01aa01a674cd9f9fe6972
Ballarja <JamesBallard87@hotmail.com>
2023-04-13 22:06:46 -0500

    adding onClick ability

--- Game_Library/src/Components/ListGroup.tsx
+++ Game_Library/src/Components/ListGroup.tsx
@@ -1,22 +1,23 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
-function ListGroup(){
-    const items = [
-        "New York",
-        "San Francisco",
-        "Tokyo",
-        "London",
-        "Paris"
-    ];
-        return (
-        <>
-            <h1>List</h1><ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
-            </ul>
-        </>
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Clicked")}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
