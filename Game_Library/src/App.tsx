2ec272d5b0f51309833a31c9ecdb2dc1b0328e7f
Ballarja <JamesBallard87@hotmail.com>
2023-04-14 21:15:43 -0500

    added onSelectItem function and handleSelection function both add functionality to onClick which logs items value to console

--- Game_Library/src/App.tsx
+++ Game_Library/src/App.tsx
@@ -2,9 +2,18 @@ import ListGroup from "./Components/ListGroup";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelection = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelection}
+      />
     </div>
   );
 }
