c1cb04d783ca96c7ba267a8d61165a8674250dc8
Ballarja <JamesBallard87@hotmail.com>
2023-04-18 20:20:06 -0500

    worked on State Structure in App.tsx

--- Game_Library/src/App.tsx
+++ Game_Library/src/App.tsx
@@ -1,37 +1,14 @@
-import ListGroup from "./Components/ListGroup";
-import Alert from "./Components/Alert";
-import Button from "./Components/Button";
-import Like from "./Components/Like";
-
 import { useState } from "react";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
-  const handleSelection = (item: string) => {
-    console.log(item);
-  };
+  const [isLoading, setLoading] = useState(false);
 
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelection}
-      />
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color="danger" onClick={() => setAlertVisibility(true)}>
-        Click Me
-      </Button>
-      <p>
-        <br></br>
-        <Like onClick={() => console.log("clicked")} />
-      </p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
