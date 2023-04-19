f809251a5cd7bad1030f408d802f113dc4bef8b2
Ballarja <JamesBallard87@hotmail.com>
2023-04-18 20:41:39 -0500

    Keeping message components pure

--- Game_Library/src/Message.tsx
+++ Game_Library/src/Message.tsx
@@ -1,7 +1,7 @@
-
-function Message() {
-    const name = "James";
-    return <h1>Hello {name}</h1>;
-}
+const Message = () => {
+  let count = 0;
+  count++;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
