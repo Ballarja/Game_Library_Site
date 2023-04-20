362f21ffaab7209979883561a27d2fd7a873754a
Ballarja <JamesBallard87@hotmail.com>
2023-04-20 16:48:36 -0500

    creating a form submission

--- Game_Library/src/App.tsx
+++ Game_Library/src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import Form from "./Components/Form";
 
 function App() {
   const handleClick = () => {};
 
-  return <div></div>;
+  return (
+    <div>
+      <Form></Form>
+    </div>
+  );
 }
 
 export default App;
